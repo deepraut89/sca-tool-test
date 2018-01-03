d60c054215fb1c2104b7e447b347cd89e3ee69de
Deepak Kumar Raut <deep.raut89@gmail.com>
2018-01-03 17:42:05 +0530

    updating .c file

--- test.c
+++ test.c
@@ -1,18 +1,9 @@
 #include<stdio.h>
-#include <stdarg.h>
-#define TEST(a,b,...) test_func(a,b, ##__VA_ARGS__)
-
-void test_func(int a, int b, ...)
+void test_func(int a, int b, int *x)
 {
-  va_list valist;
-  int *result;
-  int num=1;
   printf("Values of a and b: ");
   printf("%d %d\n",a,b);
-  va_start(valist, num);
-  result=va_arg(valist, int* );
-  printf("Var arg: %d\n",*result);
-  va_end(valist);
+  printf("Value of i in structure: %d\n",*x);
 }
 
 struct test_struct {
@@ -24,12 +15,14 @@ int main()
 {
 int a=5;
 int b=10;
+int *z;
 struct test_struct y;
 struct test_struct *x=NULL;
 printf("Inside main\n");
-y.i=5;
+y.i=15;
 printf("%d\n",y.i);
+z=&(x->i);
+test_func(a,b,z);
 printf("%d\n",x->i);
-TEST(a,b,&x->i);
 return 0;
 }
